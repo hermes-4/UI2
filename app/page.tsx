081bf7104d01db3e70f0dd48153eb74a60145f8f
hermes-4 <ebenezeroforia08@gmail.com>
2024-04-26 02:04:21 +0000

    Commit 2.22

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,13 @@
 import Side from "./ui/side";
 import To from "./ui/to";
 import Content from "./ui/content";
-import Cmobb from "./ui/content";
 
 export default function Home() {
     return (
         <main>
             <Side />
             <To />
-            <Cmobb />
+            <Content />
         </main>
     );
 }
